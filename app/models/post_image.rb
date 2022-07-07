9e33ca605762d7fa48517d68579628411a094483
nakatayudai <102974156+nakatayudai@users.noreply.github.com>
2022-07-07 09:29:46 +0000

    17finish

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,7 +2,9 @@ class PostImage < ApplicationRecord
 
   has_one_attached :image
 
-   belongs_to :user
+  belongs_to :user
+  belongs_to :post_image 
+  has_many :post_comments, dependent: :destroy
 
 
   def get_image
