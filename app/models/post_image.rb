b4ffbd591836a289922ef56392bf9f01b7921c7e
nakatayudai <102974156+nakatayudai@users.noreply.github.com>
2022-07-08 15:20:48 +0000

    21finish

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,9 +3,14 @@ class PostImage < ApplicationRecord
   has_one_attached :image
 
   belongs_to :user
-  belongs_to :post_image 
+  
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+
+  validates :shop_name, presence: true
+  validates :image, presence: true
+
 
 
   def get_image
@@ -15,9 +20,9 @@ class PostImage < ApplicationRecord
     end
     image
   end
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
 end
